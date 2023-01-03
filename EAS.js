fef36e57da5c5abd90afedce5c6564cf6b349ae3
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-01-03 14:18:12 -0600

    first commit

---
+++ EAS.js
@@ -0,0 +1,18 @@
+
+const pad = document.getElementById("container");
+const square = document.createElement('div');
+var numOfSquares = window.prompt('Number of Squares');
+
+generateDivs();
+
+
+function generateDivs()         {
+
+for     (i = 0; i < numOfSquares; i++)  {
+    document.pad.appendChild(square);
+}
+
+
+
+
+}
