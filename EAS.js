53ebaa5211597f2588956414b60bbf2654b4ffe1
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-01-03 21:00:38 -0600

    ADD GRID AND PROMPT FOR NUMBER OF SQUARES hover works but does not leave a trail. I'll add a listener tomorrow

--- EAS.js
+++ EAS.js
@@ -1,16 +1,45 @@
 
-const pad = document.getElementById("container");
-const square = document.createElement('div');
+var i = 0;
+const pad = document.querySelector("#container");
+const p = document.querySelector('p');
+
+const squareArray=[];
+
 var numOfSquares = window.prompt('Number of Squares');
 
 generateDivs();
 
 
-function generateDivs()         {
 
-for     (i = 0; i < numOfSquares; i++)  {
-    document.pad.appendChild(square);
+function generateDivs()         {
+    
+for (x = 1; x <= numOfSquares; x++) {
+   const square = document.createElement('div'  + x);
+    
+for     (i = 1; i <= numOfSquares; i++)  {
+   
+    const square = document.createElement('div' +  i);
+    
+    console.log(i);
+   
+    square.style.gridColumnStart = i;
+    square.style.gridColumnEnd = i + 1;
+    square.style.gridRowStart = x;
+    square.style.gridRowEnd =  x+1;
+    
+    square.style.color = "red";
+    //square.style.borderBlockStyle = "solid";
+    //square.style.borderBlockColor = "white";
+   // square.style.backgroundColor = "blue";
+    square.style.height = "auto";
+    square.style.width = "auto";
+ 
+    
+    pad.appendChild(square);
+    //   square.textContent = "XXX";
 }
+
+}   
 
 
 
