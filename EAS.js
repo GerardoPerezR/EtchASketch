9c012feaebaa7553d528643aa3a10a2114ccac86
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-01-04 19:54:52 -0600

    ADD SLIDER FOR NUMBER OF SQUARES This was tricky. Used replaceChildren

--- EAS.js
+++ EAS.js
@@ -1,13 +1,35 @@
 
 var i = 0;
-const pad = document.querySelector("#container");
+//const pad = document.querySelector("#pad");
+const pad = document.createElement('div');
+pad.setAttribute('id', 'pad');
+const body = document.querySelector('body');
+body.appendChild(pad);
 const p = document.querySelector('p');
 const divs = document.querySelectorAll('div');
-const squares = document.querySelectorAll('square');
 
 
-var numOfSquares = window.prompt('Number of Squares');
+const color = window.prompt("color");
 
+//var numOfSquares = 16;
+var slider = document.getElementById('numOfSquares');
+const sliderValue = document.getElementById('demo');
+const applyButton = document.getElementById('apply');
+
+
+
+
+
+applyButton.addEventListener('click', clearPad);
+
+sliderValue.innerHTML = slider.value;
+slider.oninput = function() {sliderValue.innerHTML = this.value};
+
+
+function clearPad()     {
+    pad.replaceChildren();
+    generateDivs();
+}
 
 
 
@@ -17,6 +39,8 @@ generateDivs();
 
 function generateDivs()         {
     
+var   numOfSquares = slider.value;
+
 
 
 
@@ -24,19 +48,22 @@ for (x = 0; x < numOfSquares; x++) {
    //const square = document.createElement('div'  );
     
 for     (i = 1; i <= numOfSquares; i++)  {
+
+    
+    
    
     const square = document.createElement('div' + (i + numOfSquares * x));
     
     console.log(i);
-   square.setAttribute('id', (i + numOfSquares * x)) ;
+   square.setAttribute('id', 'square' + (i + numOfSquares * x)) ;
+    square.classList.add('squares');
     square.style.gridColumnStart = i;
     square.style.gridColumnEnd = i + 1;
     square.style.gridRowStart = x;
     square.style.gridRowEnd =  x+1;
     
     square.style.color = "red";
-    //square.style.borderBlockStyle = "solid";
-    //square.style.borderBlockColor = "white";
+
    square.style.backgroundColor = "blue";
     square.style.height = "auto";
     square.style.width = "auto";
@@ -46,7 +73,8 @@ for     (i = 1; i <= numOfSquares; i++)  {
     pad.appendChild(square);
     //   square.textContent = "XXX";
 
-  square.addEventListener('mousedown', (e) => {square.style.backgroundColor = 'black'});
+  square.addEventListener('mouseover', (e) => {square.style.backgroundColor = color;
+                                                console.log(e);});
 
 }
 
